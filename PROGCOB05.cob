c48e7be70cffdcbb225960b39ac60edca6f84be5
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2023-10-18 20:47:09 -0300

    Operadores Aritméticos

--- PROGCOB05.cob
+++ PROGCOB05.cob
@@ -15,6 +15,7 @@
        77 WRK-NUM1      PIC 9(02) VALUE ZEROS.
        77 WRK-NUM2      PIC 9(02) VALUE ZEROS.
        77 WRK-RESULTADO PIC 9(04) VALUE ZEROS.
+       77 WRK-RESTO     PIC 9(02) VALUE ZEROS.
        PROCEDURE DIVISION.
            ACCEPT WRK-NUM1 FROM CONSOLE.
            ACCEPT WRK-NUM2 FROM CONSOLE.
@@ -22,6 +23,21 @@
            DISPLAY '======================='.
            DISPLAY 'NUMERO1.. ' WRK-NUM1.
            DISPLAY 'NUMERO2.. ' WRK-NUM2.
+      ******************** SOMA
            ADD WRK-NUM1 WRK-NUM2 TO WRK-RESULTADO.
-           DISPLAY 'SOMA.. ' WRK-RESULTADO.
+           DISPLAY 'SOMA........... ' WRK-RESULTADO.
+      ******************** SUBTRACAO
+           SUBTRACT WRK-NUM1 FROM WRK-NUM2 GIVING WRK-RESULTADO.
+           DISPLAY 'SUBTRACAO...... ' WRK-RESULTADO.
+      ******************** DIVISAO
+           DIVIDE WRK-NUM1 BY WRK-NUM2 GIVING WRK-RESULTADO
+             REMAINDER WRK-RESTO.
+           DISPLAY 'DIVISAO........ ' WRK-RESULTADO.
+           DISPLAY 'RESTO.......... ' WRK-RESTO.
+      ***************** MULTIPLICACAO
+           MULTIPLY WRK-NUM1 BY WRK-NUM2 GIVING WRK-RESULTADO.
+           DISPLAY 'MULTIPLICACAO.. ' WRK-RESULTADO.
+      ***************** COMPUTE
+           COMPUTE WRK-RESULTADO = (WRK-NUM1 + WRK-NUM2) / 2.
+           DISPLAY 'MEDIA.......... ' WRK-RESULTADO.
            STOP RUN.
