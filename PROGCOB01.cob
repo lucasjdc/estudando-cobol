26aeae881e601eb568a5ce58c5224524a8737608
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2023-10-18 14:38:12 -0300

    Criando nosso primero programa

---
+++ PROGCOB01.cob
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.  PROGCOB01.
+      *****************************************
+      * AREA DE COMENTARIOS - REMARKS
+      * AUTHOR  = LUCAS COSTA
+      * OBJETIVO: MOSTRAR A STRING HELLO WORLD
+      * DATA 18/10/2023
+      *****************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+           DISPLAY 'HELLO WORLD'.
+           STOP RUN.
