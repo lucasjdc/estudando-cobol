ac55840280d12db695b34c97b48b673e0d745bb0
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2023-10-18 15:33:06 -0300

    exibir somente uma parte da string

--- PROGCOB02.cob
+++ PROGCOB02.cob
@@ -12,5 +12,5 @@
        77 WRK-NOME PIC X(20) VALUE SPACES.
        PROCEDURE DIVISION.
            ACCEPT WRK-NOME FROM CONSOLE.
-           DISPLAY 'NOME: ' WRK-NOME.
+           DISPLAY 'NOME: ' WRK-NOME(1:10).
            STOP RUN.
